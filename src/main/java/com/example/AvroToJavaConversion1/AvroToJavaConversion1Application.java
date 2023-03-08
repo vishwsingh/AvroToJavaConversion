291b5f883055f1b1785cd661cbe2fd393ccb6c08
vishwsingh <khayekmusic@gmail.com>
2023-03-07 21:49:16 -0500

    Adding

---
+++ src/main/java/com/example/AvroToJavaConversion1/AvroToJavaConversion1Application.java
@@ -0,0 +1,13 @@
+package com.example.AvroToJavaConversion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroToJavaConversion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroToJavaConversion1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AvroToJavaConversion1/AvroToJavaConversion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AvroToJavaConversion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroToJavaConversion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
